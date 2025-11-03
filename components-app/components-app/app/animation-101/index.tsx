6e411ac0eb132c2f35e9f7d8fc9705faee535439
Yeampier2022 <yeampierjesusa@gmail.com>
2025-11-03 10:53:21 -0400

    feat: animatiom movi

--- components-app/components-app/app/animation-101/index.tsx
+++ components-app/components-app/app/animation-101/index.tsx
@@ -1,10 +1,11 @@
 import { useRef } from "react";
-import { View, Text, Animated } from "react-native";
+import { View, Text, Animated, Easing } from "react-native";
 import ThemedButton from "@/presentation/shared/ThemedButton";
 import ThemedView from "@/presentation/shared/ThemedView";
 
 const Animation101Screen = () => {
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(animatedOpacity, {
@@ -12,23 +13,35 @@ const Animation101Screen = () => {
       duration: 300,
       useNativeDriver: true,
     }).start();
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+      //easing: Easing.elastic(1.5)
+      easing: Easing.bounce,
+    }).start();
   };
   const fadeOut = () => {
     Animated.timing(animatedOpacity, {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start();
+    }).start(() =>
+      //  animatedTop.setValue(-100)
+
+      animatedTop.resetAnimation()
+    );
   };
 
   return (
     <ThemedView margin className="justify-center items-center flex-1">
       <Animated.View
-        className={`bg-light-secondary dark:bg-dark-secondary rounded-xl w-[150] h-[150]`}
+        className={`bg-light-secondary dark:bg-dark-secondary rounded-xl`}
         style={{
           width: 150,
           height: 150,
           opacity: animatedOpacity,
+          transform: [{ translateY: animatedTop }],
         }}
       />
 
