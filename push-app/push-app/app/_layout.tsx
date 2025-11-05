23bf20580499ffc9a0e8baa2e64fc195a3f97d85
Yeampier2022 <yeampierjesusa@gmail.com>
2025-11-05 09:40:33 -0400

    feat: start project push-app

---
+++ push-app/push-app/app/_layout.tsx
@@ -0,0 +1,29 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useThemeColor } from "@/hooks/use-theme-color";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, "background");
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: { backgroundColor: backgroundColor },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Push App" }} />
+      </Stack>
+    </ThemeProvider>
+  );
+}
---
+++ push-app/push-app/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const pushScrenn = () => {
+  return (
+    <View>
+      <Text>pushScrenn</Text>
+    </View>
+  )
+}
+
+export default pushScrenn
